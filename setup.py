2b11caf5bb0bd98d6242ef5b432934625e18f473
rnzenha-s <rzen@skl.vc>
2025-03-21 11:34:38 +0000

    Fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
     version='1.0.0',
     packages=['oculus_reader'],
     license='Apache-2.0 License',
-    long_description=open('README.md').read(),
     install_requires=[
         'numpy', 'pure-python-adb'
     ],
